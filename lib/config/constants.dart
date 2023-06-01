1c3eeae80f157b9ade10edfd54077f2255185de1
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-01 20:16:26 +0300

    Add: constants file

---
+++ lib/config/constants.dart
@@ -0,0 +1,20 @@
+class Constants {
+  static const double deviceWidth = 375;
+  static const double deviceHeight = 812;
+  static const int splashDuration = 3;
+  static const int outBoardingDurationTime = 1;
+  static const double elevationAppBar = 0;
+  static const double elevationButton = 0;
+  static const int sliderItems = 3;
+  static const int loginFirstPartFlex = 1;
+  static const int loginSacondePartFlex = 3;
+}
+
+class ConstantsPrefsKeys {
+  static const String outBoardingViewedKey = 'out_boarding_viewed';
+  static const String token = 'token';
+}
+
+class ApiConstants {
+  static const String baseUrl = 'http://act-hub.actit.ps';
+  }

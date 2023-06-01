dd2391e98d939ae51de0a9b711e96e99ad8fe3cb
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-01 20:19:07 +0300

    Add: manager strings

---
+++ lib/core/resources/manager_strings.dart
@@ -0,0 +1,14 @@
+class ManagerStrings {
+  static const String actHub = 'ActHub';
+  static const String noRoutFound = 'No Rout Found';
+  static const String skip = 'Skip';
+  static const String login = 'login';
+  static const String outBoardingTitle1 = 'Find your cozy working space 1';
+  static const String outBoardingTitle2 = 'Find your cozy working space 2';
+  static const String outBoardingTitle3 = 'Find your cozy working space 3';
+  static const String outBoardingSubTitle1 = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 1.';
+  static const String outBoardingSubTitle2 = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 2.';
+  static const String outBoardingSubTitle3 = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 3.';
+  static const String getStartButton = 'Let\'s get started';
+  static const String bearer = 'Bearer';
+}

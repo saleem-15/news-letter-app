de295084d9f3eb50fe828ab802133664bd6e910b
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-01 20:22:23 +0300

    Add: base use case

---
+++ lib/core/use_case/base_use_case.dart
@@ -0,0 +1,6 @@
+import 'package:news_letter/core/error_handler/error_handler.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class BaseUseCase<In, Out> {
+  Future<Either<Failure, Out>> execute(In input);
+}

069532867dca743478dc8cbae58c7c8a49b7ab04
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-01 20:17:14 +0300

    Adding screen util & GetMaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:news_letter/routes/routes.dart';
+
+import 'config/constants.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +14,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    // return const MaterialApp(
+    //   home: Scaffold(
+    //     body: Center(
+    //       child: Text('Hello World!'),
+    //     ),
+    //   ),
+    // );
+
+    return ScreenUtilInit(
+      splitScreenMode: true,
+      minTextAdapt: true,
+      designSize: const Size(
+        Constants.deviceWidth,
+        Constants.deviceHeight,
       ),
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: RouteGenerator.getRoute,
+          initialRoute: Routes.splashView,
+          theme: _themeService.getThemeData(),
+          themeMode: _themeService.getThemeMode(),
+        );
+      },
     );
   }
 }

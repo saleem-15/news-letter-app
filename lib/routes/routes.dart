828a470f512b02711d6ce4bea30ab6f11c2a2cbe
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-02 21:49:39 +0300

    adding splash route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,16 +1,24 @@
 import 'package:news_letter/core/resources/manager_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:news_letter/features/splash/presentation/view/splash_view.dart';
 
+import '../config/dependency_injection.dart';
 import '../features/explore/presentation/view/explore_view.dart';
 
 class Routes {
   static const String splashView = '/splash_view';
+  static const String outBoardingView = '/out_boarding_view';
   static const String exploreView = '/explore_view';
 }
 
 class RouteGenerator {
   static Route<dynamic> getRoute(RouteSettings settings) {
     switch (settings.name) {
+      case Routes.splashView:
+        initSplash();
+
+        return MaterialPageRoute(builder: (_) => const SplashView());
+
       case Routes.exploreView:
         return MaterialPageRoute(builder: (_) => const ExploreView());
 

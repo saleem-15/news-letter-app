83ffa5963a8e4edb269180e5fd10106e7604c8a5
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-02 21:52:03 +0300

    adding appName constant

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
-  static const double deviceWidth = 375;
-  static const double deviceHeight = 812;
+  static const double deviceWidth = 430;
+  static const double deviceHeight = 932;
   static const int splashDuration = 3;
   static const int outBoardingDurationTime = 1;
   static const double elevationAppBar = 0;
@@ -8,6 +8,8 @@ class Constants {
   static const int sliderItems = 3;
   static const int loginFirstPartFlex = 1;
   static const int loginSacondePartFlex = 3;
+
+  static const String appName = 'Lask';
 }
 
 class ConstantsPrefsKeys {
@@ -17,18 +19,22 @@ class ConstantsPrefsKeys {
 
 class ApiConstants {
   static const String apiKey = '8afe77481e534d1ab76a4dbf5d533508';
-   final String baseUrl = 'https://bengkelrobot.net:8003';
+  static const String baseUrl = 'https://bengkelrobot.net:8003';
 
+  static const String message = 'message';
   static const String badRequest = 'Bad Request';
 
-  // static const String authorization = 'authorization';
-  // static const int sendTimeOutDuration = 120;
-  // static const int receiveTimeOutDuration = 120;
+  static const String authorization = 'apiKey';
+  static const int sendTimeOutDuration = 120;
+  static const int receiveTimeOutDuration = 120;
   // static const String email = 'email';
   // static const String password = 'password';
   // static const String message = 'message';
   // static const String errors = 'errors';
   // static const String error = 'error';
   // static const String badRequest = 'Bad Request';
-  // static const String noInternetConnection = 'No Internet connection';
+  static const String noInternetConnection = 'No Internet connection';
+
+  static const String email = 'Bad Request';
+  static const String password = 'Bad Request';
 }

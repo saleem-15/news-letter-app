3683f0845e7f79f6711905e2f84fedf95d1874e6
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-02 22:45:36 +0300

    adding onBoarding image

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -1,6 +1,5 @@
 const String imagesPath = 'assets/images';
 
 class ManagerAssets {
-  static const String logo = '$imagesPath/logo.svg';
+  static const String onBoardingImage = '$imagesPath/onBoarding.png';
 }
-

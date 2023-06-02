af1867ce2bd30166668eea7e1bc813c988b15fea
saleem-15 <94238476+saleem-15@users.noreply.github.com>
2023-06-02 22:46:22 +0300

    adding some sizes

--- lib/core/resources/manager_sizes.dart
+++ lib/core/resources/manager_sizes.dart
@@ -4,16 +4,21 @@ class ManagerHeight {
   static double h16 = SizeUtil.setHeight(16);
   static double h24 = SizeUtil.setHeight(24);
   static double h52 = SizeUtil.setHeight(52);
+  static double h421 = SizeUtil.setHeight(421);
+  static double h538 = SizeUtil.setHeight(538);
 }
 
 class ManagerWidth {
+  static double w8 = SizeUtil.setWidth(8);
   static double w16 = SizeUtil.setWidth(16);
+  static double w24 = SizeUtil.setWidth(24);
   static double w32 = SizeUtil.setWidth(32);
 }
 
 class ManagerRadius {
   static double r32 = SizeUtil.setRadius(32);
   static double r12 = SizeUtil.setRadius(12);
+  static double r128 = SizeUtil.setRadius(128);
 }
 
 class ManagerIconSize {}
